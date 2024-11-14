06ea38eb8178c31444d5eae4493dc77ba8a32cf6
MaxIyad <hi@maxiyad.com>
2024-11-14 14:57:24 +0000

    -

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from inventory.models import Category, Ingredient
+from settings.models import Settings
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Ingredient)
+admin.site.register(Settings)
