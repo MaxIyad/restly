eadf4f3b2946110eda0e1c8cfa28e5c3e34f2d54
MaxIyad <hi@maxiyad.com>
2024-11-26 11:46:27 +0000

    =

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 
 import django_heroku
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,12 +142,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-django_heroku.settings(locals())
